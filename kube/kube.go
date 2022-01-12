c3d6b720d5edf993095c47f574fbeaa8475adaef
Alan Adi Prastyo <alan.a.prastyo@gmail.com>
2022-01-12 10:44:49 +0700

    leason 3

--- kube/kube.go
+++ kube/kube.go
@@ -1,13 +1,27 @@
 package kube
 
-type K8 struct {
+import "errors"
+
+type Kubernetes struct {
 	Deployment string
-	Service    string
-	Ingress    string
-	Pod        int
+	Pod        string
 }
 
-type Cluster struct {
-	Name     string
-	Location string
+func Name() string {
+	return "Hallo Kubernetes"
+}
+
+func Umur(byear int) (int, error) {
+	nowYear := 2022
+	age := nowYear - byear
+
+	return age, errors.New("ada error pada hitung umur")
+}
+
+func Makanan() (name string, harga int, err error) {
+	name = "nasi goreng"
+	harga = 12000
+	err = errors.New("tukang masaknya lagi pulang kampung")
+
+	return
 }
--- main.go
+++ main.go
@@ -2,23 +2,56 @@ package main
 
 import (
 	"fmt"
+	"log"
 
 	"github.com/alanadiprastyo/learn-by-code/kube"
 )
 
 func main() {
-	mykube := kube.K8{
-		Deployment: "WebService",
-		Service:    "web-service",
-		Ingress:    "webservice.apps.awancloud.io",
-		Pod:        2,
+
+	// key value [key] : [value]
+	// myMap := make(map[string]string)
+
+	// myMap["name"] = "Alan"
+	// myMap["kota"] = "Depok"
+
+	//Map tanpa make
+	// myMap := map[string]string{
+	// 	"nama": "Alan",
+	// 	"kota": "Depok",
+	// }
+
+	// map dengan interface kosong
+	// makanan := map[string]interface{}{
+	// 	"nama":  "nasi goreng",
+	// 	"harga": 15000,
+	// 	"komposisi": map[string]string{
+	// 		"gurih": "micin",
+	// 		"pedas": "cabe",
+	// 		"manis": "kecap",
+	// 	},
+	// }
+
+	// fmt.Println(fmt.Sprintf("menu makan siang hari ini adalah %s dengan harga %d dimana komposisinya adalah %s, %s, %s",
+	// 	makanan["nama"],
+	// 	makanan["harga"],
+	// 	makanan["komposisi"].(map[string]string)["gurih"],
+	// 	makanan["komposisi"].(map[string]string)["pedas"],
+	// 	makanan["komposisi"].(map[string]string)["manis"],
+	// ))
+
+	name, harga, err := kube.Makanan()
+	if err != nil {
+		log.Println(err)
+	} else {
+		fmt.Println(fmt.Sprintf("makanan hari ini adalah %s dengan harga %d", name, harga))
 	}
 
-	myCluster := kube.Cluster{
-		Name:     "Jakarta-zone-1",
-		Location: "Indonesia",
+	age, err := kube.Umur(1995)
+
+	if err != nil {
+		log.Println(err)
+	} else {
+		fmt.Println(fmt.Sprintf("umurnya adalah %d tahun", age))
 	}
-
-	fmt.Println(fmt.Sprintf("Halo Team we have Deployment %s , Service : %s, Ingress : %s, Pod : %d, running on the cluster : %s, Location : %s", mykube.Deployment, mykube.Service, mykube.Ingress, mykube.Pod, myCluster.Name, myCluster.Location))
-
 }
